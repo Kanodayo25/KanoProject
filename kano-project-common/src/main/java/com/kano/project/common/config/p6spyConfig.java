8d5a04dc9051fb5cdfe1311d9e111a366c131f34
kanokano25 <ylt8410407@163.com>
2024-07-11 16:00:30 +0800

    代码更新

--- kano-project-common/src/main/java/com/kano/project/common/config/p6spyConfig.java
+++
@@ -1,9 +0,0 @@
-package com.kano.project.common.config;
-
-import com.p6spy.engine.spy.appender.StdoutLogger;
-import org.springframework.stereotype.Component;
-
-@Component
-public class p6spyConfig extends StdoutLogger {
-
-}
