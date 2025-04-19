f3feb2ae1e4451818fc47023d539eb1db9f95434
kanodayo25 <ylt8410407@163.com>
2025-04-19 19:08:13 +0800

    sa-token

--- kano-project-parent/kano-project-provider/kano-project-provider-provider/src/main/java/com/kano/project/provider/mapper/RoleMapper.java
+++ kano-project-parent/kano-project-provider/kano-project-provider-provider/src/main/java/com/kano/project/provider/mapper/RoleMapper.java
@@ -3,5 +3,5 @@ package com.kano.project.provider.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.kano.project.provider.entity.Role;
 
-public class RoleMapper extends BaseMapper<Role> {
+public interface RoleMapper extends BaseMapper<Role> {
 }
